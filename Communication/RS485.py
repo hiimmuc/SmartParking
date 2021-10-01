68e71b6fa898ead4030bb58c800695ea596337b9
MuC <nam1811.hust@gmail.com>
2021-10-01 17:31:27 +0700

    add commication part

--- Communication/RS485.py
+++ Communication/RS485.py
@@ -1,1 +1,67 @@
 #TODO: connect with plc via rs485, modbus rtu
+
+from easymodbus.modbusClient import ModbusClient
+
+class RS485():
+	def __init__(self, port="COM0", is_rtu=True, ip=None):
+		self.connected = False
+		self.port = port 
+		self.ip = ip
+		self.is_rtu = is_rtu
+
+    def write(self, type_, address, value):
+        try:
+            #  check connection
+            try:
+                if self.is_rtu:
+                    plc = ModbusClient(f'COM{self.port}')
+                else:
+                    plc = ModbusClient(self.ip, self.port)
+            except Exception as e:
+                print("Error", e)
+            if not plc.is_connected():
+                plc.connect()
+            if plc.is_connected():
+                print("PlC is connected, writing_time")
+                self.connected = True
+
+            if type_ == 'coil':
+                plc.write_single_coil(address, value)
+            elif type_ == 'reg':
+                plc.write_single_register(address, value)
+        except Exception as e:
+            self.popup_msg(msg=e, src_msg='write_to_PLC_core', type_msg='warning')
+            self.connected = False
+
+    def read(self, type_, address):
+        """read data from plc with type and address defined
+
+        Args:
+            type_ (str): type of reading functions. Available: hr, ir, coil.
+            address (int): address of reading type. eg. 1, 2, 3.
+
+        Returns:
+            [list]: list of results
+        """
+        try:
+            if self.is_rtu:
+                plc = ModbusClient(f'COM{self.port}')
+            else:
+                plc = ModbusClient(self.ip, self.port)
+
+            if not plc.is_connected():
+                plc.connect()
+            if type_.strip() == 'hr':
+                return plc.read_holdingregisters(address, 1)[0]
+
+            if type_.strip() == 'ir':
+                return plc.read_inputregisters(address, 1)[0]
+
+            if type_.strip() == 'coil':
+                return plc.read_coils(address, 1)[0]
+        except Exception as e:
+            self.popup_msg(e, src_msg='read_from_PLC')
+
+
+
+
--- Communication/UART.py
+++ Communication/UART.py
@@ -1,2 +1,21 @@
 #TODO: connect with arduino with uart serial communication, also use for reading RFID
 import serial
+
+class UART():
+	def __init__(self, port, baudrate=9600, timeout=0):
+		self.rfid_serial_port = erial.Serial(port, baudrate, timeout)
+
+	def read_id(self):
+		id_num = []
+		i = 0
+		while True:
+		    serial_data = self.rfid_serial_port.read()
+		    data = serial_data.decode('utf-8')
+		    i = i + 1
+		    if i == 12:
+		        i = 0
+		        ID = "".join(map(str, id_num))
+		        print(ID)
+		    else:
+		        id_num.append(data)
+		return id_num
