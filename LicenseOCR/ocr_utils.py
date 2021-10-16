1c077ad1a5ac77ffb1c301a89fd7c9cf41e3fd70
Hiimmuc <you@example.comnam1811.hust@gmail.com>
2021-10-16 18:26:20 +0700

    tiny change

--- LicenseOCR/ocr_utils.py
+++ LicenseOCR/ocr_utils.py
@@ -11,7 +11,7 @@ def get_grayscale(image):
 
 
 def remove_noise(image):
-    return cv2.medianBlur(image, 5)
+    return cv2.medianBlur(image, 3)
 
 # thresholding
 
@@ -23,14 +23,14 @@ def thresholding(image):
 
 
 def dilate(image):
-    kernel = np.ones((5, 5), np.uint8)
+    kernel = np.ones((3, 3), np.uint8)
     return cv2.dilate(image, kernel, iterations=1)
 
 # erosion
 
 
 def erode(image):
-    kernel = np.ones((5, 5), np.uint8)
+    kernel = np.ones((3, 3), np.uint8)
     return cv2.erode(image, kernel, iterations=1)
 
 # opening - erosion followed by dilation
@@ -50,12 +50,17 @@ def canny(image):
 
 
 def deskew(image):
-    coords = np.column_stack(np.where(image > 0))
+    # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.bitwise_not(image)
+    thresh = cv2.threshold(gray, 0, 255,
+                           cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+    coords = np.column_stack(np.where(thresh > 0))
     angle = cv2.minAreaRect(coords)[-1]
     if angle < -45:
         angle = -(90 + angle)
     else:
-        angle = -angle
+        angle = 0
     (h, w) = image.shape[:2]
     center = (w // 2, h // 2)
     M = cv2.getRotationMatrix2D(center, angle, 1.0)
@@ -67,3 +72,14 @@ def deskew(image):
 
 def match_template(image, template):
     return cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
+
+
+def preprocess(image):
+    gray = get_grayscale(image)
+    gray = remove_noise(gray)
+    thresh = thresholding(gray)
+    dil = dilate(thresh)
+    erode_img = erode(dil)
+    deskew_img = deskew(erode_img)
+    rgb = cv2.cvtColor(deskew_img, cv2.COLOR_GRAY2RGB)
+    return rgb
--- test.py
+++ test.py
@@ -11,6 +11,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from GUI.gui_utils import *
 from LicenseDetection.Yolo_LP import *
 from LicenseOCR.ocr import *
+from LicenseOCR.ocr_utils import *
 
 
 def gui():
@@ -25,7 +26,8 @@ def model():
     # Choose random motorbike image from the dataset
     num = random.randint(0, 2489)
     image_name = "xemay" + str(num) + ".jpg"
-    img_path = os.path.join(r"backup\Number Plate Dataset\yolo_plate_dataset", image_name)
+    # img_path = os.path.join(r"backup\Number Plate Dataset\yolo_plate_dataset", image_name)
+    img_path = r"LicenseOCR\test image\bien-so-xe-4-so-xau.jpg"
 
     # Yolo
     t0 = time.time()
@@ -37,12 +39,18 @@ def model():
     print("Load OCR model time:", time.time() - t1)
     # detect
     t2 = time.time()
-    LP = Yolo_engine.yolo_LP(img_path)
+    plate = Yolo_engine.yolo_LP(img_path)
     print("Detect time:", time.time() - t2)
-    plt.imshow(LP)
+
+    f, axarr = plt.subplots(1, 2)
+    axarr[0].imshow(plate)
+
+    # # preprocess
+    # plate = preprocess(plate)
+    # axarr[1].imshow(plate)
     # ocr
     t3 = time.time()
-    print(ocr.read(LP, 'cv2', return_confidence=True), "\nPredict time:", time.time() - t3)
+    print(ocr.read(img_path, 'cv2', return_confidence=True), "\nPredict time:", time.time() - t3)
     plt.show()
 
 
