4c5f22321e6a33033c13c060a831d9a34d1056a2
Hiimmuc <you@example.comnam1811.hust@gmail.com>
2021-10-28 22:36:07 +0700

    add some things

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ lưu vào csv ID / biển / số tiền còn
 - code comunication để giao tiếp rs485, uart
 - code gui để hiển thị 2 khung ảnh, 1 table theo dõi truyền nhận, 1 box hiện thông tin nhận diện, 2 button thủ công đề phòng sai sót
 
+model weight
+https://drive.google.com/file/d/1gdKWeb6EFlpq8sQtu8CTiL1mtHfFtL0Y/view?usp=sharing
--- test.py
+++ test.py
@@ -1,20 +1,8 @@
-import random
-import sys
-import time
-import typing
-
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-
-from LicenseDetection import Yolo_LP
 from LicenseRecognition import *
 
 
 def model():
     # Choose random motorbike image from the dataset
-    img_path = r"LicenseOCR\test image\bien-so-xe-4-so-xau.jpg"
-
     recognizer = LicenseRecognizer()
 
     recognizer.video_detect()
